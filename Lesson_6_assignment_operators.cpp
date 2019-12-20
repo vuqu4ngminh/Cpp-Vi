9c61879aba152cccf90b0d82390bba017581c381
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:08:16 +0700

    Bài 6

---
+++ Lesson_6_assignment_operators.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// phép gán cơ bản (= += -= *= /= %=)
+	int b = 10;
+	cout << "b = 10" << endl;
+	cout << "Nhập giá trị a" << endl;
+	int a;
+	cin >> a;
+	
+	a += b;
+	cout << "Phep gán += của b cho a là :" << a << endl;
+	a -= b;
+	cout << "Phep gán -= của b cho a là :" << a << endl;
+	a *= b;
+	cout << "Phep gán *= của b cho a là :" << a << endl;
+	a %= b;
+	cout << "Phep gán %= của b cho a là :" << a << endl;
+	return 0;
+}
