91de1498040e346ea17851be122ca012e4db34e7
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:07:30 +0700

    Bài 5

---
+++ Lesson_5_math.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// math
+	int a = 3;
+	int b = 4;
+
+	int sum = a + b;
+	int sub = a - b;
+	int mul = a * b;
+	int div = a / b;
+	int mod = a % b;
+	
+	cout << sub << endl;
+	cout << mul << endl;
+	cout << div << endl;
+	cout << mod << endl;
+	cout << sum << endl;
+	return 0;
+}
