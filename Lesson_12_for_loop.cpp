9764c5e7ea8cc1ceb13354c36fd3c86d5c27a87b
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:10:57 +0700

    Bài 12

---
+++ Lesson_12_for_loop.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* vòng lặp for
+		Cấu trúc:
+		for (Khỏi tạo;Điều kiện;Bước nhảy)
+		{ 
+			// nội dung thực hiện
+		}
+	 */
+	int giaithua = 1;
+	int n;
+	cout << "Tinh giai thừa !" << endl;
+	cout << "Nhập giá trị n: ";
+	cin >> n;
+	if (n < 0)
+	{
+		cout << "n không thể là số âm" << endl;
+	}
+	else
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			giaithua *= i;
+		}
+		cout << n << "! = " << giaithua << endl;
+	}
+	
+	return 0;
+}
