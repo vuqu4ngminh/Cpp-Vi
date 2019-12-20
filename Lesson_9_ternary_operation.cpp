4657c720de9f79b7b77aa73aabb7773c95cb73ef
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:09:35 +0700

    Bài 9

---
+++ Lesson_9_ternary_operation.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* toán tử ba ngôi
+	   tổng quát : biến = (điều kiện) ? biểu thức 1 : biểu thức 2 ;
+	*/
+
+	int a;
+	int b;
+	cout << "Nhập giá trị a" << endl;
+	cin >> a;
+	cout << "Nhập giá trị b" << endl;
+	cin >> b;
+
+	string x;
+	x = (a > b) ? "a > b" : " a < b" ;
+	cout << x << endl;
+	return 0;
+}
