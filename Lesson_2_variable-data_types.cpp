967458f68d426a48d1d465c258e823fc790a19ba
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:05:55 +0700

    Bài 2

---
+++ Lesson_2_variable-data_types.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// variable
+	int var1 = 1;
+	float var2 = 2.5f;
+	cout << var1 << " and " << var2 << endl;
+
+	// data types
+	
+	//character
+	char a = 'b';
+	//integer
+	int b = 1;
+	// float
+	float c = 3.33f;
+	// double
+	double d = 3.14524356;
+	// boolean (true/false)
+	bool e = false;
+	// ngoài ra còn void và wide character
+	cout << a << endl;
+	cout << b << endl;
+	cout << c << endl;
+	cout << d << endl;
+	cout << e << endl;
+	// ở kiểu bool giá trị true false sẽ in ra màn hình với ký tự 0 (false) và 1 (true)
+
+	return 0;
+}
