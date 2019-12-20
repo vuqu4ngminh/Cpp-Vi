0ee10c5bd7bc31819826c11258925931433d0916
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:09:11 +0700

    Bài 8

---
+++ Lesson_8_if-else.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// câu lệnh if else
+	// phép so sánh ( > < >= <= == != )
+
+	// ví dụ cơ bản
+	cout << "Nhập giá trị cua a" << endl;
+	int a;
+	cin >> a ;
+	cout << "Nhập giá trị b" << endl;
+	int b;
+	cin >> b ;
+	if (a > b)
+	{
+		cout << "Vậy a > b" << endl;
+	}
+	else
+	{
+		cout << "Vậy a < b" << endl;
+	}
+
+	return 0;
+}
