fd02b9498d90419adf2853003e2d3537f528e4d6
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:06:28 +0700

    Bài 3

---
+++ Lesson_3_constants.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define b 2					// cách 1
+
+int main()
+{
+	// hằng số
+	const int a = 1;		// cách 2
+	
+	cout << a << " and " << b << endl;
+	return 0;
+}
