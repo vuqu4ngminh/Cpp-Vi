84031b514a7ddce02bc6d8b1021158e73ae16e7e
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:13:50 +0700

    Bài 19

---
+++ Lesson_19_pointers.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Con trỏ
+	
+	int num = 3;
+
+	// tham chiếu
+	cout << "Địa chỉ vùng nhớ của num: " << &num << endl;
+
+	// trỏ tới địa chỉ
+	int* newNum = &num;					// biến mới sử dụng chung địa chỉ trong bộ nhớ với biến cũ
+	cout << "Địa chỉ vùng nhớ của newNum: " << newNum << endl;
+	
+	// in kết quả
+	cout << num << endl;
+	cout << *newNum << endl;
+
+	return 0;
+}
