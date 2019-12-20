01cc16635b384d0c28f677d9e1fde1deee42b5c5
vuqu4ngminh <minh66771507@gmail.com>
2019-12-20 17:12:39 +0700

    Bài 16

---
+++ Lesson_16_array(n2).cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	/*
+		Mảng động:
+		Cú pháp: <kiểu dữ liệu> * <tên mảng> = new <kiểu dữ liệu>[ <số phần tử> ]
+		Thường dùng khi người dùng cần nhập dữ liệu , tránh việc lãng phí hay thiếu tài nguyên máy
+	*/
+	
+	// tạo mảng động
+	int* a = new int[100];
+
+	// nhập dữ liệu vào mảng
+	for (size_t i = 0; i < 10; i++)
+	{
+		cin >> a[i];
+	}
+	
+	// in dữ liệu
+	for (size_t i = 0; i < 10; i++)
+	{
+		cout << a[i] << " ";
+	}
+	return 0;
+}
